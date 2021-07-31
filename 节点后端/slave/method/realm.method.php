ec042a7d952e955aa0d280cd0b5d3e12fdc17050
xieruan <59883140+xieruan@users.noreply.github.com>
2021-08-01 00:49:36 +0800

    Add files via upload

---
+++ 节点后端/slave/method/realm.method.php
@@ -0,0 +1,40 @@
+<?php
+
+use PortForward\Base;
+
+class PortForward_realm
+{
+    public $rules;
+    public $b;
+    public function __construct()
+    {
+        $this->b = new Base();
+    }
+
+    public function PortForward_realm_create($data)
+    {        
+        $this->b->run('nohup realm -l 127.0.0.1:'.$data['nodeport'].' -r '.$data['forwardip'].':'.$data['forwardport'].' > /dev/null 2>&1&');
+        return true;
+    }
+
+    public function PortForward_realm_delete($data)
+    {
+        $this->b->run("kill $(ps aux |grep realm\ -l\ 127.0.0.1:".$data['nodeport']." | sed '/grep/d' | awk '{print $2}')");
+        return true;
+    }
+
+    public function PortForward_realm_olddelete($data)
+    {
+        $this->b->run("kill $(ps aux |grep realm\ -l\ 127.0.0.1:".$data['nodeport']." | sed '/grep/d' | awk '{print $2}')");
+        return true;
+    }
+
+    public function PortForward_realm_checkRepeat($data)
+    {
+        $res = $this->b->run("ps aux |grep realm\ -l\ 127.0.0.1:".$data['nodeport']."| sed '/grep/d' | awk '{print $2}'");
+        if (!empty(trim($res," "))) {
+            return true;
+        }
+        return false;
+    }
+}
