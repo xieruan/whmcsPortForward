e4b5cfc447be45d163784b25d78b9d28ece66ceb
xieruan <59883140+xieruan@users.noreply.github.com>
2021-08-02 00:49:51 +0800

    Update installc.sh

--- installc.sh
+++ installc.sh
@@ -4,4 +4,4 @@ export PATH
 echo -e " ${Tip} 正在安装ehco..."
 wget https://mirror.ghproxy.com/https://github.com/Ehco1996/ehco/releases/download/v1.0.7/ehco_1.0.7_linux_amd64 -O /usr/bin/ehco && chmod +x /usr/bin/ehco
 echo -e " ${Tip} 完毕退出"
-done
+exit
