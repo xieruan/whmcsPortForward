9a7c5b3c082d90c60d3bd02524528e26720383c3
xieruan <59883140+xieruan@users.noreply.github.com>
2021-08-01 17:06:58 +0800

    Add files via upload

---
+++ 节点后端/slave/method/ehco.method.php
@@ -0,0 +1,40 @@
+<?php
+
+use PortForward\Base;
+
+class PortForward_ehco
+{
+    public $rules;
+    public $b;
+    public function __construct()
+    {
+        $this->b = new Base();
+    }
+
+    public function PortForward_ehco_create($data)
+    {        
+        $this->b->run('nohup ehco -l 127.0.0.1:'.$data['nodeport'].' -lt ws -r '.$data['forwardip'].':'.$data['forwardport'].' > /dev/null 2>&1&');
+        return true;
+    }
+
+    public function PortForward_ehco_delete($data)
+    {
+        $this->b->run("kill $(ps aux |grep ehco\ -l\ 127.0.0.1:".$data['nodeport']." | sed '/grep/d' | awk '{print $2}')");
+        return true;
+    }
+
+    public function PortForward_ehco_olddelete($data)
+    {
+        $this->b->run("kill $(ps aux |grep ehco\ -l\ 127.0.0.1:".$data['nodeport']." | sed '/grep/d' | awk '{print $2}')");
+        return true;
+    }
+
+    public function PortForward_ehco_checkRepeat($data)
+    {
+        $res = $this->b->run("ps aux |grep ehco\ -l\ 127.0.0.1:".$data['nodeport']."| sed '/grep/d' | awk '{print $2}'");
+        if (!empty(trim($res," "))) {
+            return true;
+        }
+        return false;
+    }
+}
