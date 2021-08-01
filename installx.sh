768859abbb2f1f30bb2207e729b6aec8f5ab0301
xieruan <59883140+xieruan@users.noreply.github.com>
2021-08-01 18:11:04 +0800

    Update installx.sh

--- installx.sh
+++ installx.sh
@@ -37,6 +37,7 @@ Install() {
 		cat /etc/redhat-release |grep 7\..*|grep -i centos>/dev/null
 		if [[ $? = 1 ]]; then
 			echo -e " ${Error} 不支持Centos6/8，请更换Centos 7 x64" && exit 1 
+		fi
 		echo -e " ${Tip} 正在安装php7.0..."
 		rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 		rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
