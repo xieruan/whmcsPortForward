b8cbde77f680026fbc1e955a7072cda861e4daf7
xieruan <59883140+xieruan@users.noreply.github.com>
2021-08-02 12:18:29 +0800

    Update ehco.method.php

--- 节点后端/slave/method/ehco.method.php
+++ 节点后端/slave/method/ehco.method.php
@@ -13,25 +13,25 @@ class PortForward_ehco
 
     public function PortForward_ehco_create($data)
     {        
-        $this->b->run('nohup ehco -l 127.0.0.1:'.$data['nodeport'].' -lt ws -r '.$data['forwardip'].':'.$data['forwardport'].' > /dev/null 2>&1&');
+        $this->b->run('nohup ehco -l 0.0.0.0:'.$data['nodeport'].' -lt ws -r '.$data['forwardip'].':'.$data['forwardport'].' > /dev/null 2>&1&');
         return true;
     }
 
     public function PortForward_ehco_delete($data)
     {
-        $this->b->run("kill $(ps aux |grep ehco\ -l\ 127.0.0.1:".$data['nodeport']." | sed '/grep/d' | awk '{print $2}')");
+        $this->b->run("kill $(ps aux |grep ehco\ -l\ 0.0.0.0:".$data['nodeport']." | sed '/grep/d' | awk '{print $2}')");
         return true;
     }
 
     public function PortForward_ehco_olddelete($data)
     {
-        $this->b->run("kill $(ps aux |grep ehco\ -l\ 127.0.0.1:".$data['nodeport']." | sed '/grep/d' | awk '{print $2}')");
+        $this->b->run("kill $(ps aux |grep ehco\ -l\ 0.0.0.0:".$data['nodeport']." | sed '/grep/d' | awk '{print $2}')");
         return true;
     }
 
     public function PortForward_ehco_checkRepeat($data)
     {
-        $res = $this->b->run("ps aux |grep ehco\ -l\ 127.0.0.1:".$data['nodeport']."| sed '/grep/d' | awk '{print $2}'");
+        $res = $this->b->run("ps aux |grep ehco\ -l\ 0.0.0.0:".$data['nodeport']."| sed '/grep/d' | awk '{print $2}'");
         if (!empty(trim($res," "))) {
             return true;
         }
